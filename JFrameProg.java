e8a918311169a09a73c9cb544507f35841f91442
jagdish gupta <varshneyjacky@gmail.com>
2023-10-17 23:20:07 +0530

    -a

--- JFrameProg.java
+++ JFrameProg.java
@@ -1,5 +1,15 @@
+import javax.swing.JFrame;
+
 public class JFrameProg{
     public static void main(String[] args) {
+        JFrame jf = new JFrame();
+      //  jf.setBounds(20, 20, 200, 400);   we define size here also
+
+
+        jf.setLayout(null);
+        jf.setSize(200,300);  // here we define the opening size of jframe
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
     }
 }
