0de6ae90c9089d25a4c144d722e716aac54854d5
jagdish gupta <varshneyjacky@gmail.com>
2023-10-20 22:27:53 +0530

    -a

--- JLabelProg.java
+++ JLabelProg.java
@@ -3,12 +3,13 @@ import javax.swing.JFrame;
 public class JLabelProg {
     public static void main(String[] args) {
         JFrame jf= new JFrame();
+        jf.setBounds(200, 200, 400, 400);
 
 
 
 
 
-        
+
         jf.setSize(500, 300);
         jf.setLayout(null);
         jf.setVisible(true);
