1f1d7fa9596d7520d8df2128f86b32b2d99f115f
jagdish gupta <varshneyjacky@gmail.com>
2023-10-21 00:07:35 +0530

    -a

--- JLabelProg.java
+++ JLabelProg.java
@@ -1,22 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class JLabelProg {
     public static void main(String[] args) {
         JFrame jf= new JFrame();
-        jf.setBounds(200, 200, 400, 400);
-
-               
-
-
-
-
-
-
-        jf.setSize(500, 300);
+        jf.setBounds(100, 100, 1000, 500);
         jf.setLayout(null);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        Container c= jf.getContentPane();
+        c.setLayout(null);
+
+        //to set image into JLabel..........
+        ImageIcon icon= new ImageIcon("hi.avif");
+        JLabel jl= new JLabel("hello",icon,JLabel.RIGHT);
+        jl.setBounds(10, 50, icon.getIconWidth(), icon.getIconHeight());
+
+        // to change font of jlabel
+        Font f= new Font("Arial",Font.ITALIC,20);
+        jl.setFont(f);
+        
+        
+
+
+        c.add(jl);
+
+
         
     }
     
