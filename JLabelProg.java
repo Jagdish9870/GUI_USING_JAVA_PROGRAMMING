1c672dfbc3727fce42143895eed7671f4d56ecb5
jagdish gupta <varshneyjacky@gmail.com>
2023-10-20 22:26:01 +0530

    -a

--- JLabelProg.java
+++ JLabelProg.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class JLabelProg {
     public static void main(String[] args) {
+        JFrame jf= new JFrame();
+
+
+
+
+
+        
+        jf.setSize(500, 300);
+        jf.setLayout(null);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
     }
     
