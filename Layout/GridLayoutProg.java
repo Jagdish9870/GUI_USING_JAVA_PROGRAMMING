eb953009f6b805e0d02e42530801efc3878bf46c
jagdish gupta <varshneyjacky@gmail.com>
2023-10-29 10:28:06 +0530

    -a

--- Layout/GridLayoutProg.java
+++ Layout/GridLayoutProg.java
@@ -1,5 +1,7 @@
 package Layout;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -7,25 +9,33 @@ public class GridLayoutProg {
     public static void main(String[] args) {
         JFrame jf = new JFrame();
          jf.setBounds(200, 200, 600, 400);  
-         jf.setLayout(null);
+        
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JButton jb= new JButton("1");
-        jb.setBounds(20, 20, 80, 50);
+      
         JButton jb2= new JButton("2");
-        jb2.setBounds(120, 20, 80, 50);
+       
         JButton jb3= new JButton("3");
-        jb3.setBounds(220, 20, 80, 50);
+     
         JButton jb4= new JButton("4");
-        jb4.setBounds(20, 120, 80, 50);
+       
         JButton jb5= new JButton("5");
-        jb5.setBounds(120, 120, 100, 50);
+       
+
         jf.add(jb);
         jf.add(jb2);
         jf.add(jb3);
         jf.add(jb4);
         jf.add(jb5);
+        GridLayout g= new GridLayout();
+        g.setHgap(10);
+        g.setVgap(10);
+
+        // to set number of row and col to fit component and we can also set the gap
+        //  GridLayout g= new GridLayout(2,3,10,20);
+         jf.setLayout(g);
 
         
     }
